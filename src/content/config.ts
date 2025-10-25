8d357bfd78d93cfe9f501624998d5ee6d0a7bcfc
John Gustavo Choque Condori <johnchque.tech@gmail.com>
2025-10-25 15:20:41 +0200

    Adding Egypt photos.

--- src/content/config.ts
+++ src/content/config.ts
@@ -64,8 +64,39 @@ const worldCollection = defineCollection({
   }),
 });
 
+const worksCollection = defineCollection({
+  loader: glob({ pattern: "*.md", base: "./src/content/works" }),
+  //   type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date().or(z.string()).transform((val) => new Date(val)).optional(),
+    // category: z
+    //   .enum([
+    //     "Learning",
+    //     "Seedling",
+    //     "Budding",
+    //     "Evergreen",
+    //     "Now",
+    //     "Reflections",
+    //     "World"
+    //   ])
+    //   .optional(),
+    // date: z
+    //   .date()
+    //   .or(z.string())
+    //   .transform((val) => new Date(val)),
+    // updated: z
+    //   .date()
+    //   .or(z.string())
+    //   .transform((val) => new Date(val))
+    //   .optional(),
+    description: z.string().optional(),
+    // tags: z.array(z.string()).optional(),
+  }),
+});
 
 export const collections = {
   notes: notesCollection,
   world: worldCollection,
+  works: worksCollection,
 };
